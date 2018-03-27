95edbe4ce764d78e0a8224e7b5895f036e7b6428
KeyListener <15205902@ucdconnect.ie>
2018-03-28 00:55:48 +0800

    Update question_generate_test.py

--- question_generate/question_generate_test.py
+++ question_generate/question_generate_test.py
@@ -7,34 +7,12 @@ import jieba.posseg as pseg
 #def title_changing(sentence):
 #	words = pseg.cut(sentence)
 #	for word, flag in words:
-#		if(flag == 'r'):
-#			print(sentence+'？')
-#			break
-
-
-def single_def_line_changing(sentence):
-	words = pseg.cut(sentence)
-	for word, flag in words:
-		if word == '是':
-			print('是什么？')
-			break
-		elif word == '：' or word == ':':
-			print('是什么？')
-			break
-		print(word, end = '')
-
-def single_guide_line_changing(sentence):
+#		
 	words = pseg.cut(sentence)
 #	i = 0
 #	v = 0
 	word_list = []
-	for word, flag in words:
-#		i = i + 1
-		if flag == 'v' or flag == 'vn' or flag == 'vd':
-			word_list = []
-		word_list.append(word)
-
-	print('怎么', end = '')
+	for 
 	for x in word_list:
 		print(x, end = '')
 
